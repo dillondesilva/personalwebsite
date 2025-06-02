c3fb75769cfe25685938f766bcc9fb82de5e6cde
dillondesilva <dillon.desilva@geeksdiary.com>
2025-06-02 15:28:02 +1000

    Updated with link to article

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
-  <div>
+  <div class="">
     <h1>Dillon de Silva</h1> 
   </div>
 `
